ff99b33d76aa309c1ba074cea8133d1e81193b53
nhnam0209 <93809958+nhnam0209@users.noreply.github.com>
2022-03-03 08:58:02 +0700

    update

--- landingpage/src/component/Intro/Intro.jsx
+++ landingpage/src/component/Intro/Intro.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import "./intro.scss"
 
+
 export default function Intro() {
   return (
     <div className='intro' id = 'intro'>
-      <div className="slider">
-        <div className="container">
+      <div className="container">
           <div className="wrapper">
-            <h1>Welcome to Bun Vit 281 LVS</h1>
-            <h2>It's pleasure to server</h2>
-          </div>
+            <div className="slider">        
+              <h1>Welcome to Bun Vit 281 LVS</h1>
+              <button id = "contact">Contact to Order</button>
+            </div>
         </div>
       </div>
     </div>
